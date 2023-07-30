4b44930ad8b76cabda8544cb487ae2b3fa9b6938
Kauan Victor <k4uan222@gmail.com>
2023-07-30 17:11:55 -0300

    Listando os pokemons na tela

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    
+    <div class="column is-half is-offset-one-quarter">
+      <div v-for="(poke, index) in pokemons" :key="index">
+       <Pokemon :name="poke.name" :url="poke.url" :num="index + 1"  /> 
+    </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios"
+import Pokemon from "./components/Pokemon"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      pokemons: []
+    }
+  },
+ created:function(){
+  axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+    this.pokemons = res.data.results
+    console.log(this.pokemons)
+  })
+ },
+ components: {
+  Pokemon
+ }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="main-pokemon"> 
+      <div class="card">
+      <div class="card-image">
+        <figure>
+        <img :src="pokemon.front" alt="Placeholder image">
+      </figure>
+      </div>
+      <div class="card-content">
+      <div class="media">
+        
+        <div class="media-content">
+          <p class="title is-4">{{upperName(name)}}</p>
+          <p class="subtitle is-6">{{pokemon.type}}</p>
+        </div>
+      </div>
+
+      <div class="content">
+       
+      </div>
+      </div>
+     </div>
+
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  name: 'Pokemon',
+  created: function(){
+    axios.get(this.url).then(res => {
+      this.pokemon.type = res.data.types[0].type.name;
+      this.pokemon.front = res.data.sprites.front_default;
+      this.pokemon.back = res.data.sprites.back_default;
+    })
+  },  
+  data(){
+    return{
+      pokemon: {
+        type: '',
+        front: '',
+        back: ''
+      }
+    }
+  },
+  props: {
+    name: String,
+    url: String,
+    num: Number
+  }, 
+  methods: {
+    upperName: function(value){
+      let newName = value[0].toUpperCase() + value.slice(1)
+      return newName
+    }
+  }
+}
+</script>
+
+<style>
+  #main-pokemon{
+    margin-top: 2%;
+  }
+</style>
